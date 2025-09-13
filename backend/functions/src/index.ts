a02752ae8ab47d329b5c23c56149bf1ec4d29ac4
riatalwar <rtalwar@silk-shopping.com>
2025-09-13 02:09:38 -0400

    updates

--- backend/functions/src/index.ts
+++ backend/functions/src/index.ts
@@ -7,14 +7,14 @@
  * See a full list of supported triggers at https://firebase.google.com/docs/functions
  */
 
-import {setGlobalOptions} from "firebase-functions";
-import {onRequest} from "firebase-functions/v2/https";  // Use v2 for better performance
+import {
+  onRequest,
+} from "firebase-functions/v2/https"; // Use v2 for better performance
 import {setGlobalOptions} from "firebase-functions/v2/options";
-import * as logger from "firebase-functions/logger";
 
-import app from './app';
+import app from "./app";
 
 // Set global options
-setGlobalOptions({ maxInstances: 10 });
+setGlobalOptions({maxInstances: 10});
 
 export const api = onRequest(app);
